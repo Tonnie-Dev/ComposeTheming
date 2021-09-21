36413c3dd985e96c0a4a0337055d6dfd8245ba41
Antony <vontonnie@gmail.com>
2021-09-21 08:05:01 +0300

    Clip Icon Shape

--- app/src/main/java/com/codelab/theming/ui/start/Home.kt
+++ app/src/main/java/com/codelab/theming/ui/start/Home.kt
@@ -27,20 +27,26 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.ZeroCornerSize
 import androidx.compose.material.*
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.rounded.Palette
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
+import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shape
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.semantics.heading
 import androidx.compose.ui.semantics.semantics
+import androidx.compose.ui.text.AnnotatedString
 import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.withStyle
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -163,20 +169,36 @@ private fun PostMetadata(
     post: Post,
     modifier: Modifier = Modifier
 ) {
+
     val divider = "  •  "
     val tagDivider = "  "
+    val tagStyle = MaterialTheme.typography.overline.toSpanStyle()
+        .copy(background = MaterialTheme.colors.primary.copy(alpha = 0.1f))
+
     val text = buildAnnotatedString {
         append(post.metadata.date)
         append(divider)
         append(stringResource(R.string.read_time, post.metadata.readTimeMinutes))
         append(divider)
         post.tags.forEachIndexed { index, tag ->
+
+            //spacing tags
             if (index != 0) {
                 append(tagDivider)
             }
-            append(" ${tag.uppercase(Locale.getDefault())} ")
+
+            /*Pushes style to the AnnotatedString.Builder, executes block
+            and then pops the style.*/
+            withStyle(tagStyle) {
+
+                append(" ${tag.uppercase()} ")
+            }
+
         }
     }
+
+
+
 
     CompositionLocalProvider(LocalContentAlpha provides ContentAlpha.medium) {
 
@@ -203,6 +225,7 @@ fun PostItem(
         icon = {
             Image(
                 painter = painterResource(post.imageThumbId),
+                   modifier = modifier.clip(shape = MaterialTheme.shapes.small),
                 contentDescription = null
             )
         },
@@ -256,5 +279,36 @@ private fun FeaturedPostPreview() {
 private fun HomePreview() {
     Home()
 }
+
+@Preview("Normal")
+@Composable
+private fun NormalTextPreview() {
+    Text(text = "GOAT - uyu ukumete haha ni ninie")
+}
+
+@Preview("Overline")
+@Composable
+private fun OverlinePreview() {
+    Text(text = "GOAT - uyu ukumete haha ni ninie", style = MaterialTheme.typography.overline)
+}
+
+
+@Composable
+fun FilledTextField(
+
+
+    shape: Shape = MaterialTheme.shapes.small.copy(
+        bottomStart = ZeroCornerSize,
+        bottomEnd = ZeroCornerSize
+    )
+) {
+
+    val (text, setText) = remember { mutableStateOf("") }
+    TextField(value = text, onValueChange = setText, shape = shape)
+}
+
+@Preview( name = "TextField")
+@Composable
+fun FilledTextFieldPreview() = FilledTextField()
 
 
